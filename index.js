3f547bf3ec340db5292bfdb41bbb30be50eed8ff
tuthoang <hoangt@bu.edu>
2017-10-24 18:20:01 -0400

    node and ejs

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 var express = require('express')
 var path = require('path')
 var cors = require('cors');
+const bodyParser = require('body-parser')
 const Twit = require('twit')
 const app = express()
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors())
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
 app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views'))
+app.set('views', path.join(__dirname , 'views'))
 
-var j
 var T = new Twit({
   consumer_key:         'A15zwJ94dD4puhGr6QBQKxWsd',
   consumer_secret:      '0udsXOyiGrSt3i6lze0TQeNKaCmqlc4hzujMDCDGMXZ9QIVfgB',
@@ -22,12 +19,19 @@ var T = new Twit({
   access_token_secret:  '4j4ynkCV2QtSYIpok3wXoy09NrjA0WwdYJmTQCh83v9aT'
 })
 
+var j
 app.get('/', function (req, res) {
-  T.get('search/tweets', { q: 'perrydBUCS', count: 100 }, function(err, data, response) {
-  console.log(data)
-  j = data;
+  res.render('index')
 })
-  res.render('index',
+
+app.post('/search', function (req, res){
+  var searchContents = req.body.searchBar;
+  console.log(searchContents)
+  T.get('users/search', { q: searchContents, count: 100 }, function(err, data, response) {
+    // console.log(data)
+    j = data;
+  })
+  res.render('search',
   {
     twitter: j
   })
