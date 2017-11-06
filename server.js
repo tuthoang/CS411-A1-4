f953df46437e4485ac4e9713c2db882c8a85f5f0
tuthoang <hoangt@bu.edu>
2017-11-05 21:43:34 -0500

    fixed index page

--- server.js
+++ server.js
@@ -26,6 +26,6 @@ app.get('/', function (req, res) {
   res.render('index')
 })
 
-app.listen(8890, function () {
+app.listen(8889, function () {
   console.log('Example app listening on port 8890!')
 })
