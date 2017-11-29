552e1998186b2ba8ec5d6f40b12eb3e7adc35786
tuthoang <hoangt@bu.edu>
2017-11-28 22:47:53 -0500

    added jwt auth

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,8 +1,14 @@
 const TwitterStrategy = require('passport-twitter').Strategy;
 const LocalStrategy = require('passport-local').Strategy;
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
 var User = require('../models/Users')
 
 const config = require('./config')
+
+
+// opts.issuer = 'accounts.examplesoft.com';
+// opts.audience = 'yoursite.net';
 
 module.exports = function(passport) {
 
@@ -20,40 +26,58 @@ module.exports = function(passport) {
   //     done(null, profile);
   //   }
   // ));
-
-  // Use local strategy
-  passport.use(new LocalStrategy({
-    usernameField: "email",
-    passwordField: "password",
-    passReqToCallback : true // allows us to pass back the entire request to the callback
-  },function(req, email, password, done) {
-    User.findOne({
-      email: email
-    }, function(err, user) {
-      // When an error occurred
+  var opts = {};
+  opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+  opts.secretOrKey = config.JWTsecret.secret;
+  passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
+    console.log(jwt_payload);
+    User.findOne({_id: jwt_payload.id}, function(err, user) {
       if (err) {
-        return done(err);
+        console.log('err');
+        return done(err, false);
       }
-      
-      // When user not found 
-      if (!user) {
-        return done(null, false, {
-          message: 'Unknown user'
-        });
+      if (user) {
+        console.log(user);
+        done(null, user);
+      } else {
+        console.log('no user');
+        done(null, false);
       }
-
-      // When the password is invalid
-      user.comparePassword(password, function(err,isMatch){
-        if(err) throw err;
-        if(isMatch){
-          console.log('logged');
-          return done(null,user);
-        } else{
-          return done(null,false, {message:'Invalid password'});
-        }
-      })
     });
   }));
+  // Use local strategy
+  // passport.use(new LocalStrategy({
+  //   usernameField: "email",
+  //   passwordField: "password",
+  //   passReqToCallback : true // allows us to pass back the entire request to the callback
+  // },function(req, email, password, done) {
+  //   User.findOne({
+  //     email: email
+  //   }, function(err, user) {
+  //     // When an error occurred
+  //     if (err) {
+  //       return done(err);
+  //     }
+      
+  //     // When user not found 
+  //     if (!user) {
+  //       return done(null, false, {
+  //         message: 'Unknown user'
+  //       });
+  //     }
+
+  //     // When the password is invalid
+  //     user.comparePassword(password, function(err,isMatch){
+  //       if(err) throw err;
+  //       if(isMatch){
+  //         console.log('logged');
+  //         return done(null,user);
+  //       } else{
+  //         return done(null,false, {message:'Invalid password'});
+  //       }
+  //     })
+  //   });
+  // }));
 
   passport.serializeUser(function(user, done) {
     console.log('in serialize, setting id on session:', user.id)
--- server/routes/api.js
+++ server/routes/api.js
@@ -4,6 +4,7 @@ const config = require('../config/config');
 const passport = require('passport');
 var User = require('../models/Users');
 
+const jwt = require('jsonwebtoken');
 const Twit = require('twit');
 const indico = require('indico.io');
 
@@ -77,13 +78,56 @@ router.post('/create', (req,res) => {
   });
 })
 
-router.post('/login', passport.authenticate('local', {successRedirect:'/api/me', failureRedirect: '/api', failureFlash: true, session:true}),
-  function(req,res) {
-    // console.log('sucess')
-    req.flash('success_msg', 'logged in');
-    res.redirect('/');
-  })
+// router.post('/login', passport.authenticate('local', {successRedirect:'/api/me', failureRedirect: '/api', failureFlash: true, session:true}),
+//   function(req,res) {
+//     // console.log('sucess')
+//     req.flash('success_msg', 'logged in');
+//     res.redirect('/');
+//   })
 
 
+// router.post('/login', passport.authenticate('jwt', {session:false}),
+//   function(req,res) {
+//     // console.log('sucess')
+//     req.flash('success_msg', 'logged in');
+//     res.redirect('/api/me');
+//   })
+router.post("/login", function(req, res) {
+  if(req.body.email && req.body.password){
+    var email = req.body.email;
+    var password = req.body.password;
+  }
+
+  User.findOne({email: email}, function(err, user) {
+    if (err) {
+        return res.json({message: "error"})
+    }
+    if(!user) res.json({message:"error: user not found"});
+
+     else {
+      user.comparePassword(password, function(err,isMatch){
+      if(err) throw err;
+      if(isMatch){
+        var payload = {id: user._id};
+        var token = jwt.sign(payload, config.JWTsecret.secret);
+        res.json({message: "ok", token: 'Bearer ' + token});
+      } else{
+        res.json({message:'Invalid password'});
+      }
+    })
+    }
+  });
+});
+router.get("/secret", passport.authenticate('jwt', { session: false }), function(req, res){
+  res.json({message:"Success! You can not see this without a token"});
+});
+
+router.get("/secretDebug",
+  function(req, res, next){
+    console.log(req.get('Authorization'));
+    next();
+  }, function(req, res){
+    res.json("debugging");
+});
 
 module.exports = router;
