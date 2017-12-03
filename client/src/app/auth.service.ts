28e85350ed43cb110d8f60915c798afc53bb9a3c
tuthoang <hoangt@bu.edu>
2017-12-03 18:33:58 -0500

    route

--- client/src/app/auth.service.ts
+++ client/src/app/auth.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class AuthService {
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient,private router: Router) {}
 
   login(credentials) {
     interface JSONdata{
@@ -16,18 +17,26 @@ export class AuthService {
       .subscribe(
         // We're assuming the response will be an object
         // with the JWT on an id_token key
-        data => localStorage.setItem('jwt', data.token),
-        error => console.log(error)
+        data => {
+            if(data.token!=null){
+              localStorage.setItem('jwt', data.token);
+              this.router.navigate(['search-twitter']);
+            }
+        },
+        error => console.log(error),
       );
   }
+
   getToken(): string{
     console.log(localStorage.getItem('jwt'));
     return localStorage.getItem('jwt');
   }
+
   logout() {
     localStorage.removeItem('jwt');
     // localStorage.removeItem("expires_at");
   }
+
   public isAuthenticated(): boolean {
     // get the token
     const token = this.getToken();
--- client/src/app/greeting/greeting.component.ts
+++ client/src/app/greeting/greeting.component.ts
@@ -3,6 +3,7 @@ import { HttpParams, HttpClient, HttpHeaders } from '@angular/common/http';
 import { FormBuilder, FormGroup, FormControl, Validators, NgForm } from '@angular/forms';
 import { AuthService } from '../auth.service';
 import { PasswordValidation } from './password-validation.validator';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'greeting',
@@ -23,7 +24,7 @@ export class GreetingComponent  {
   matchPW: boolean = false;
   emailFocus: boolean = false;
   message: Object;
-  constructor(private auth: AuthService,private fb: FormBuilder, public http: HttpClient){
+  constructor(private auth: AuthService,private fb: FormBuilder, public http: HttpClient, public router: Router){
     this.rForm = fb.group({
       'email' : [null, Validators.compose([Validators.email,Validators.required])],
       'password' : [null, Validators.compose([Validators.required,Validators.minLength(5)])],
@@ -40,8 +41,9 @@ export class GreetingComponent  {
       );
   }
   Login(form:NgForm){
-    if(form.value. email && form.value.password)
+    if(form.value. email && form.value.password){
       this.auth.login(form.value);
+    }
   }
 
 }
